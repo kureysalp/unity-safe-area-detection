234bfc198ad94cffd2186571088debce87f6a073
Alp <kureysalp@gmail.com>
2020-08-06 10:32:02 +0300

    first commit

---
+++ SafeAreaDetection.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SafeAreaDetection : MonoBehaviour
+{
+    public delegate void SafeAreaChanged(Rect safeArea);
+    public static event SafeAreaChanged OnSafeAreaChanged;
+
+    private Rect safeArea;
+
+    private void Awake()
+    {
+        safeArea = Screen.safeArea;
+    }
+
+    private void Update()
+    {
+        if (safeArea != Screen.safeArea)
+        {
+            safeArea = Screen.safeArea;
+            OnSafeAreaChanged?.Invoke(safeArea);
+        }
+    }
+}
